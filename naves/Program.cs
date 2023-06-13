19dcb6d77669d7cb97e38051125430ff1e93c09c
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-06-13 02:15:13 -0300

    Añadir prueba de Forms

--- naves/Program.cs
+++ naves/Program.cs
@@ -9,11 +9,11 @@ namespace naves {
 			Game.Target = GameTarget.Console;
 			Game.Debug = true;
 
-			CGUI.PrepareConsole("Juego de naves");
-			CGUI.GameArea = new Rect(1, 1, 59, 23);
-			CGUI.DrawBasicUI();
+			GUI.PrepareConsole("Juego de naves");
+			GUI.GameArea = new Rect(1, 1, 59, 23);
+			DrawBasicUI();
 
-			Player player = new Player(new Vec2(CGUI.GameCenter, CGUI.GameBottom - 6));
+			Player player = new Player(new Vec2(GUI.GameCenter, GUI.GameBottom - 6));
 			Game.AddInstance(player);
 			StageSequencer sequencer = new StageSequencer(new List<Spawner>());
 			Game.AddInstance(sequencer);
@@ -25,24 +25,56 @@ namespace naves {
 
 		private static void PromptGameStart() {
 			string bottomPrompt = "Comenzar...";
-			int leftPos = CGUI.CenteredStringOffset(CGUI.GameRight + 1, CGUI.UIRight, bottomPrompt);
-			Console.SetCursorPosition(leftPos, CGUI.GameBottom - 1);
+			int leftPos = GUI.CenteredStringOffset(GUI.GameRight + 1, GUI.UIRight, bottomPrompt);
+			Console.SetCursorPosition(leftPos, GUI.GameBottom - 1);
 			Console.Write(bottomPrompt);
 			Console.ReadKey(true);
 			bottomPrompt = "Esc. para salir ";
-			leftPos = CGUI.CenteredStringOffset(CGUI.GameRight + 1, CGUI.UIRight, bottomPrompt);
+			leftPos = GUI.CenteredStringOffset(GUI.GameRight + 1, GUI.UIRight, bottomPrompt);
 			Console.CursorLeft = leftPos;
 			Console.Write(bottomPrompt);
 		}
 
 		private static void ShowGameOver() {
 			Console.ResetColor();
-			CGUI.DrawBasicUI();
-			CGUI.EmptyGameFrame();
-			Console.SetCursorPosition(CGUI.GameCenter, CGUI.GameMiddle);
+			DrawBasicUI();
+			GUI.EmptyGameFrame();
+			Console.SetCursorPosition(GUI.GameCenter, GUI.GameMiddle);
 			Console.Write("- FIN DEL JUEGO -");
 			Thread.Sleep(3000);
 			Console.ReadKey(true);
+		}
+
+		private static void DrawBasicUI() {
+			//Líneas horizontales
+			string hbar1 = new string('═', GUI.GameRight);
+			string hbar2 = new string('═', GUI.UIRight - GUI.GameRight - 2);
+			Console.WriteLine("╔" + hbar1 + "╦" + hbar2 + "╗");
+			Console.CursorTop = 24;
+			Console.WriteLine("╚" + hbar1 + "╩" + hbar2 + "╝");
+			Console.CursorTop = 0; //Evita scrollear hacia abajo
+
+			//Líneas verticales
+			GUI.DrawVerticalLine(new Vec2(0, 1), GUI.GameBottom, '║');
+			GUI.DrawVerticalLine(new Vec2(GUI.GameRight + 1, 1), GUI.GameBottom, '║');
+			GUI.DrawVerticalLine(new Vec2(GUI.UIRight, 1), GUI.GameBottom, '║');
+
+			//Panel vertical
+			Console.BackgroundColor = ConsoleColor.DarkBlue;
+			int panelWidth = GUI.UIRight - GUI.GameRight - 2;
+			string filler = new string(' ', panelWidth);
+			for(int i = 1; i < GUI.GameBottom - GUI.GameTop + 2; i++) {
+				Console.SetCursorPosition(GUI.GameRight + 2, i);
+				Console.Write(filler);
+			}
+
+			string diffName = "~NORMAL~";
+			Console.CursorTop = 2;
+			Console.CursorLeft = GUI.CenteredStringOffset(GUI.GameRight + 1, GUI.UIRight, diffName);
+			Console.BackgroundColor = ConsoleColor.Cyan;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.Write(diffName);
+			Console.ResetColor();
 		}
 	}
 }
---
+++ pruebaforms/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameCore;
+
+namespace pruebaforms {
+	public partial class Form1: Form {
+		public Form1() {
+			this.InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			Game.Target = GameTarget.Forms;
+			Game.Debug = true;
+
+			GUI.PrepareConsole("Juego de naves");
+			GUI.GameArea = new Rect(1, 1, 59, 23);
+
+			Game.Loop();
+		}
+	}
+}
